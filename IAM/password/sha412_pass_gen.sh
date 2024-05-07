e83e4d5ab55ce82938e2e370444c16f1cc9fc3c6
Nicole Ferland <nicole.garrow64@gmail.com>
2024-05-07 16:05:59 -0400

    aa

--- IAM/password/sha412_pass_gen.sh
+++ IAM/password/sha412_pass_gen.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#if on ubuntu make sure to install... --> apt-get install pwgen
+#--> apt install whois
 pass=`pwgen --secure --capitalize --numerals --symbols 12 1`
 
 echo $pass | mkpasswd --stdin --method=sha-512; 
